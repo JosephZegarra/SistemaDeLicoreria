2cd7a11b4aeed04314ab278cc8b1aca1011e9c39
Estephany <esurcoa@unsa.edu.pe>
2022-10-13 22:01:15 -0500

    Modifique el archivo heroes

--- src/main/java/Heroes.java
+++ src/main/java/Heroes.java
@@ -1,5 +1,10 @@
 public class Heroes {
     public String nombres;
-  
+    public Boolean usaCapa;
     public  int numero_de_poderes;
+
+    public String  toString(){
+	    return "el heroe "+ nombres+" tiene "+numero_poderes+" y si usa capa es"+usaCapa;
+    
+    }
 }
